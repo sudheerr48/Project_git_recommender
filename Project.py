fc6898014a852374e9cddd0942dfa1d904fa6150
Naga Sudheer Ravela <sudheerr48@gmail.com>
2017-10-12 22:10:25 -0500

    Update Project.py

--- Project.py
+++ Project.py
@@ -9,19 +9,82 @@ Created on Thu Oct 12 17:28:52 2017
 # Import necessary modules
 import pickle
 import networkx as nx
+from nxviz import ArcPlot
 from nxviz import CircosPlot
 import matplotlib.pyplot as plt
 from itertools import combinations
 from collections import defaultdict
 
 
-
 #loaded pickle file into a variable G
 G = pickle.load( open( "/Users/nagasudheerravela/Desktop/github_users.p", "rb" ) )
 
 
+# Plot the degree distribution of the GitHub collaboration network
+plt.hist(list(nx.degree_centrality(G).values()))
+plt.show()
 
 
+# Plot the degree distribution of the GitHub collaboration network
+plt.hist(list(nx.betweenness_centrality(G).values()))
+plt.show()
+
+
+# Find the author(s) that are part of the largest maximal clique: largest_clique
+largest_clique = sorted(nx.find_cliques(G), key=lambda x:len(x))[-1]
+
+# Create the subgraph of the largest_clique: G_lc
+G_lc = G.subgraph(largest_clique)
+
+# Create the CircosPlot object: c
+c = CircosPlot(G_lc)
+
+# Draw the CircosPlot to the screen
+c.draw()
+plt.show()
+
+# Calculate the maximal cliques in G: cliques
+cliques = nx.find_cliques(G)
+
+# Count and print the number of maximal cliques in G
+print(len(list(cliques)))
+
+
+
+# Compute the degree centralities of G: deg_cent
+deg_cent = nx.degree_centrality(G)
+
+# Compute the maximum degree centrality: max_dc
+max_dc = max((deg_cent).values())
+
+# Find the user(s) that have collaborated the most: prolific_collaborators
+prolific_collaborators = [n for n, dc in deg_cent.items() if dc == max_dc]
+
+# Print the most prolific collaborator(s)
+print(prolific_collaborators)
+
+
+# Identify the largest maximal clique: largest_max_clique
+largest_max_clique = set(sorted(nx.find_cliques(G), key=lambda x: len(x))[-1])
+
+# Create a subgraph from the largest_max_clique: G_lmc
+G_lmc = G.subgraph(largest_max_clique)
+
+# Go out 1 degree of separation
+for node in G_lmc.nodes():
+    G_lmc.add_nodes_from(G.neighbors(node))
+    G_lmc.add_edges_from(zip([node]*len(G.neighbors(node)), G.neighbors(node)))
+
+# Record each node's degree centrality score
+for n in G_lmc.nodes():
+    G_lmc.node[n]['degree centrality'] = nx.degree_centrality(G_lmc)[n]
+        
+# Create the ArcPlot object: a
+a = ArcPlot(G_lmc, node_order='degree centrality')
+
+# Draw the ArcPlot to the screen
+a.draw()
+plt.show()
 
 # Initialize the defaultdict: recommended
 recommended = defaultdict(int)

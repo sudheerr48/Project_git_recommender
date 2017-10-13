1525f531eea898a6122f32215f95e14b769548d3
Naga Sudheer Ravela <sudheerr48@gmail.com>
2017-10-12 22:08:06 -0500

    Update and rename netw.py to Basics.py

--- Basics.py
+++ Basics.py
@@ -2,14 +2,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Oct 11 18:10:19 2017
-
+Thanks to Eric Ma for tutorial on nxviz
 @author: nagasudheerravela
 """
 import pickle
-
 import networkx as nx
 import matplotlib.pyplot as plt
-# Import nxviz
 from nxviz.plots import ArcPlot
 from nxviz import MatrixPlot
 from itertools import combinations
@@ -20,35 +18,35 @@ from itertools import combinations
 G = pickle.load( open( "/Users/nagasudheerravela/Desktop/github_users.p", "rb" ) )
 
 #printing the type of variable G
-#print(type(G))
+print(type(G))
 
 #plotting the network
-#nx.draw(G)
-#plt.show()
+nx.draw(G)
+plt.show()
 
 
 #printing nodes in graph
-#print(G.nodes())
+print(G.nodes())
 
 #printing number of total nodes in graph
-#print(len(list(G.nodes())))
+print(len(list(G.nodes())))
 
 #printing edges in graph
-#print(len(list(G.edges())))
+print(len(list(G.edges())))
 
 #printing first 10 nodes data with metadata
-#nodes_1_10 = [n for n,d in G.nodes(data=True) ]
+nodes_1_10 = [n for n,d in G.nodes(data=True) ]
 
 #Plotting Graph using matrix plot
 
 # Create the MatrixPlot object: m
-#m = nv.MatrixPlot(G)
+m = nv.MatrixPlot(G)
 
 # Draw m to the screen
-#m.draw()
+m.draw()
 
 # Display the plot
-#plt.show()
+plt.show()
 
 #To check whether my graph has any self nodes 
 # Define find_selfloop_nodes()
@@ -70,8 +68,8 @@ def find_number_selfloop_nodes(G):
     return nodes_in_selfloops
 
 # Check whether number of self loops equals the number of nodes in self loops
-#print(len(find_number_selfloop_nodes(G)))
-#print(G.number_of_selfloops() )
+print(len(find_number_selfloop_nodes(G)))
+print(G.number_of_selfloops() )
 
 #To findout each neighbour nodes
 # Define nodes_with_m_nbrs()
@@ -94,30 +92,30 @@ def nodes_with_m_nbrs(G,m):
     return nodes
 
 #For finding nodes having only one neighbour
-#OneNeighbour_nodes = nodes_with_m_nbrs(G,1)
-#print(OneNeighbour_nodes)
+OneNeighbour_nodes = nodes_with_m_nbrs(G,1)
+print(OneNeighbour_nodes)
     
 
-#def List_nodes_with_max_nbrs(G):
+def List_nodes_with_max_nbrs(G):
     """
     Returns all nodes in graph G that have maximum count of neighbors.
     """
-    #nodes_by_neighborcount = [None]*len(list(G.nodes()))
+    nodes_by_neighborcount = [None]*len(list(G.nodes()))
     
     # Iterating over all nodes in G
-    #for n in range(len(list(G.nodes()))):
+    for n in range(len(list(G.nodes()))):
           
         # checking node neighbour count i am starting from 1 than 0 
-        #if ( n != 0 ):
+        if ( n != 0 ):
             
             #Adding nodes having same number of neighbors in one list and adding all those lists together  
-           # nodes_by_neighborcount[n] = nodes_with_m_nbrs(G,n)
+           nodes_by_neighborcount[n] = nodes_with_m_nbrs(G,n)
             
              
-       #     max_nmbr = len(nodes_by_neighborcount[n])
-    #return  nodes_by_neighborcount
-# try to use this function only if u have a good RAM  
-#List_check = List_nodes_with_max_nbrs(G)
+           max_nmbr = len(nodes_by_neighborcount[n])
+    return  nodes_by_neighborcount
+# try to use this function only if u have a good RAM  because it is taking a lot of time 
+List_check = List_nodes_with_max_nbrs(G)
  
     
     
@@ -129,77 +127,77 @@ def nodes_with_m_nbrs(G,m):
 degrees = [len(G.neighbors(n)) for n in G.nodes()]
 
 # Print the degrees
-#print(degrees)
+print(degrees)
  
 #maximum degree calculation
 
-#Max_degree = max(degrees) 
-#print(Max_degree)
+Max_degree = max(degrees) 
+print(Max_degree)
 
 #nodes having maximum degee
-#m = []
-#Max_node = [i for i in G.nodes() if len(G.neighbors(i)) == Max_degree]
+m = []
+Max_node = [i for i in G.nodes() if len(G.neighbors(i)) == Max_degree]
 
-#print(Max_node)
+print(Max_node)
 
 # Compute the degree centrality of the Github network: deg_cent
-#deg_cent = nx.degree_centrality(G)
+deg_cent = nx.degree_centrality(G)
 
 # Plot a histogram of the degree centrality distribution of the graph
-#plt.figure()
-#plt.ylabel('Degree centre values')
-#plt.xlabel('nodes')
-#plt.hist(list(deg_cent.values()))
-#plt.show()
+plt.figure()
+plt.ylabel('Degree centre values')
+plt.xlabel('nodes')
+plt.hist(list(deg_cent.values()))
+plt.show()
 
 # Plot a histogram of the degree distribution of the graph
-#plt.figure()
-#plt.ylabel('Degree values')
-#plt.xlabel('nodes')
-#plt.hist(degrees)
-#plt.show()
+plt.figure()
+plt.ylabel('Degree values')
+plt.xlabel('nodes')
+plt.hist(degrees)
+plt.show()
 
 # Plot a scatter plot of the centrality distribution and the degree distribution
-#plt.figure()
-#plt.xlabel('degrees')
-#plt.ylabel('Degree centre values')
-#plt.scatter(degrees, list(deg_cent.values()))
-#plt.show()
+plt.figure()
+plt.xlabel('degrees')
+plt.ylabel('Degree centre values')
+plt.scatter(degrees, list(deg_cent.values()))
+plt.show()
 
 #obseravation:- we can clearly observe positive correlation between degrees and degree centrality of network
 
 # Compute the betweenness centrality of G: bet_cen
-#bet_cen = nx.betweenness_centrality(G)
+bet_cen = nx.betweenness_centrality(G)
 # Create a scatter plot of betweenness centrality and degree centrality
-#plt.scatter(list(bet_cen.values()) ,list(deg_cent.values()))
+plt.scatter(list(bet_cen.values()) ,list(deg_cent.values()))
 # Display the plot
-#plt.show()
+plt.show()
 
 
 
 
 # Calculate the largest connected component subgraph: largest_ccs
-#largest_ccs = sorted(nx.connected_component_subgraphs(G), key=lambda x: len(x))[-1]
+largest_ccs = sorted(nx.connected_component_subgraphs(G), key=lambda x: len(x))[-1]
 
 # Create the customized MatrixPlot object: h
-#h = MatrixPlot(graph = largest_ccs )
+h = MatrixPlot(graph = largest_ccs )
 
 # Draw the MatrixPlot to the screen
-#h.draw()
-#plt.show()
+h.draw()
+plt.show()
 
 # Iterate over all the nodes in G, including the metadata
-#for n, d in G.nodes(data=True):
+for n, d in G.nodes(data=True):
 
     # Calculate the degree of each node: G.node[n]['degree']
-    #G.node[n]['degree'] = nx.degree(G,n)
+    G.node[n]['degree'] = nx.degree(G,n)
     
 # Create the ArcPlot object: a
-#a = ArcPlot(graph = G ,node_order = 'degree')
+a = ArcPlot(graph = G ,node_order = 'degree')
 
 # Draw the ArcPlot to the screen
-#a.draw()
-#plt.show()
+a.draw()
+plt.show()
 
 
 
